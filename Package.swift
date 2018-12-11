673b8762e2860ac1636d07a17f2d84e8fb6bb0de
Jimmy McDermott <mcdappdev@gmail.com>
2018-12-11 00:55:25 -0500

    add library

--- Package.swift
+++ Package.swift
@@ -4,6 +4,11 @@ import PackageDescription
 
 let package = Package(
     name: "RedisJobs",
+    products: [
+        .library(
+            name: "RedisJobs",
+            targets: ["RedisJobs"]),
+    ],
     dependencies: [
         .package(url: "https://github.com/vapor-community/jobs.git", from: "0.0.1"),
         .package(url: "https://github.com/vapor/redis.git", from: "3.0.0")
