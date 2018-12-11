d5a25ff95d4127e95667537c716b29d001f78452
Jimmy McDermott <mcdappdev@gmail.com>
2018-12-10 21:40:55 -0500

    add documentation

--- Sources/RedisJobs/main.swift
+++ Sources/RedisJobs/main.swift
@@ -1,10 +1,18 @@
-@_exported Jobs
-import Jobs
+@_exported import Jobs
 import Redis
 import Foundation
+import NIO
 
-//TODO: - Move this into a separate redis package
 extension RedisDatabase: JobsPersistenceLayer {
+    
+    /// Stores the job in Redis with the specified data
+    ///
+    /// - Parameters:
+    ///   - key: The key to store the data
+    ///   - job: The `Job` to store
+    ///   - maxRetryCount: The number of retries to
+    ///   - worker: An `EventLoopGroup` that can be used to generate future values
+    /// - Returns: A future `Void` value used to signify completion
     public func set<J: Job>(key: String, job: J, maxRetryCount: Int, worker: EventLoopGroup) -> EventLoopFuture<Void> {
         return self.newConnection(on: worker).flatMap(to: RedisClient.self) { conn in
             let jobData = JobData(key: key, data: job, maxRetryCount: maxRetryCount)
@@ -15,6 +23,12 @@ extension RedisDatabase: JobsPersistenceLayer {
         }
     }
     
+    /// Returns the job data using rpoplpush
+    ///
+    /// - Parameters:
+    ///   - key: The key to retrieve the data from
+    ///   - worker: An `EventLoopGroup` that can be used to generate future values
+    /// - Returns: A future `Void` value used to signify completion
     public func get(key: String, worker: EventLoopGroup) -> EventLoopFuture<JobData?> {
         let processingKey = key + "-processing"
         
@@ -29,6 +43,14 @@ extension RedisDatabase: JobsPersistenceLayer {
         }
     }
     
+    /// Left blank because Redis does not need a cleanup with this implementation.
+    /// See `JobsPersistenceLayer`.`completed` for a full description
+    ///
+    /// - Parameters:
+    ///   - key: The key that was used to complete the job
+    ///   - jobString: The string representation of the job
+    ///   - worker: An `EventLoopGroup` that can be used to generate future values
+    /// - Returns: A future `Void` value used to signify completion
     public func completed(key: String, jobString: String, worker: EventLoopGroup) -> EventLoopFuture<Void> {
         return worker.future()
     }
