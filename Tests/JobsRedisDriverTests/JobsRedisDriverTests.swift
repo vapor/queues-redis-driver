6ea7805b5cb90966600ff332699d563f5cf7ed37
Jimmy McDermott <mcdappdev@gmail.com>
2019-01-23 18:04:03 -0500

    add warning test

--- Tests/JobsRedisDriverTests/JobsRedisDriverTests.swift
+++ Tests/JobsRedisDriverTests/JobsRedisDriverTests.swift
@@ -1,13 +1,30 @@
 import XCTest
 import class Foundation.Bundle
+import JobsRedisDriver
+import Redis
+import NIO
+import Jobs
 
 final class JobsRedisDriverTests: XCTestCase {
     
-    func testWarningExists() {
+    func testWarningExists() throws {
+        guard let url = URL(string: "redis://localhost:6379") else { return }
+        let database = try RedisDatabase(url: url)
+        let el = MultiThreadedEventLoopGroup(numberOfThreads: 1).next()
+        let redisInstance = JobsRedisDriver(database: database, eventLoop: el)
         
+        let job = EmailJob()
+        try redisInstance.set(key: "key", job: job, maxRetryCount: 1).wait()
     }
 
     static var allTests = [
         ("testWarningExists", testWarningExists),
     ]
 }
+
+struct EmailJob: Job {
+    func dequeue(context: JobContext, worker: EventLoopGroup) -> EventLoopFuture<Void> {
+        return worker.future()
+    }
+}
+
