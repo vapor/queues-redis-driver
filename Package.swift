21b0c4312026d726931d2c30f29c175b1816ad33
Jimmy McDermott <mcdappdev@gmail.com>
2019-06-11 11:29:05 -0500

    add circle

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.1
 
 import PackageDescription
 
@@ -10,7 +10,7 @@ let package = Package(
             targets: ["JobsRedisDriver"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor-community/jobs.git", from: "0.2.0"),
+        .package(url: "https://github.com/vapor-community/jobs.git", from: "1.0.0-alpha.1.0"),
         .package(url: "https://github.com/vapor/redis.git", from: "3.0.0")
     ],
     targets: [
