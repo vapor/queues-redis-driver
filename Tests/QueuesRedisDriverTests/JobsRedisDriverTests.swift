67c42024677f70b8e4f298c9063dc8326040830f
Andrew Edwards <andrewangeta@gmail.com>
2021-03-30 23:03:49 -0400

    Added test for encoding date as epoch.

--- Tests/QueuesRedisDriverTests/JobsRedisDriverTests.swift
+++ Tests/QueuesRedisDriverTests/JobsRedisDriverTests.swift
@@ -58,6 +58,36 @@ final class JobsRedisDriverTests: XCTestCase {
         XCTAssertEqual(job.jobName, "FailingJob")
         _ = try redis.delete(RedisKey(id)).wait()
     }
+    
+    func testDateEncoding() throws {
+        let app = Application(.testing)
+        defer { app.shutdown() }
+
+        app.queues.add(DelayedJob())
+
+        try app.queues.use(.redis(url: "redis://\(hostname):6379"))
+
+        app.get("delay-job") { req in
+            req.queue.dispatch(DelayedJob.self, .init(name: "vapor"),
+                               delayUntil: Date(timeIntervalSince1970: 1609477200)) // Jan 1, 2021
+                .map { HTTPStatus.ok }
+        }
+
+        try app.testable().test(.GET, "delay-job") { res in
+            XCTAssertEqual(res.status, .ok)
+        }
+        
+        // Verify the delayUntil date is encoded as the correct epoch time
+        let redis = (app.queues.queue as! RedisClient)
+        let keys = try redis.send(command: "KEYS", with: ["*".convertedToRESPValue()]).wait()
+        let id = keys.array!.filter { $0.string!.hasPrefix("job:") }[0].string!
+        let job = try redis.get(RedisKey(id)).wait()
+        let dict = try JSONSerialization.jsonObject(with: job.data!, options: .allowFragments) as! [String: Any]
+        
+        XCTAssertEqual(dict["jobName"] as! String, "DelayedJob")
+        XCTAssertEqual(dict["delayUntil"] as! Int, 1609477200)
+        _ = try redis.delete(RedisKey(id)).wait()
+    }
 }
 
 var hostname: String {
@@ -78,6 +108,19 @@ final class Email: Job {
     func dequeue(_ context: QueueContext, _ message: Message) -> EventLoopFuture<Void> {
         self.sent.append(message)
         context.logger.info("sending email \(message)")
+        return context.eventLoop.makeSucceededFuture(())
+    }
+}
+
+final class DelayedJob: Job {
+    struct Message: Codable, Equatable {
+        let name: String
+    }
+    
+    init() {}
+    
+    func dequeue(_ context: QueueContext, _ message: Message) -> EventLoopFuture<Void> {
+        context.logger.info("Hello \(message.name)")
         return context.eventLoop.makeSucceededFuture(())
     }
 }
